78263505b1d7a732f73ea74994af75edf5259ba6
ANDRIW AMORIM SANTAREM <70485336+andriwamorimsan@users.noreply.github.com>
2021-11-01 11:56:31 -0300

    já feito o bootstrap

---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listar Usuários</title>
+    <link rel="stylesheet" href="{{asset('site/bootstrap.css')}}">
+</head>
+<body>
+        <h1>Quantidade de Usuarios: {{ count($data) }}</h1>
+        <a href="{{ route('user.create')}} "> Cadastrar Usuário</a>
+
+        @foreach($data as $item)
+            <ul>
+                <li>
+                    {!! $item->id !!}
+                </li>
+                <li>
+                    {!! $item->name !!}
+                </li>
+                <li>
+                    {!! $item->email !!}
+                </li>
+                <li>
+                    {!! $item->password !!}
+                </li>
+                <li>
+                    <a href="{{ url('/user/show',$item->id) }}"  >Editar</a>
+                   <!--  <a href="{{ route('user.show',$item->id) }}">Editar</a> -->
+                </li>
+                <li>
+                    <a href="{{ route('user.delete',$item->id) }}">Deletar</a>
+                </li>
+
+
+            </ul>
+        @endforeach
+
+<script src="{{asset('site/jquery.js')}}"></script>
+<script src="{{asset('site/bootstrap.js')}}"></script>
+</body>
+</html>
